591c8d7e4eab93d74101f703805773f4d806c699
GraziMarinoni <grazi.marinoni@gmail.com>
2023-11-07 12:30:03 +0000

    Adjusting style and carousel

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const topMovieURL =
 const topSevenURL =
   "http://localhost:8000/api/v1/titles/?imdb_score_min=9.3&sort_by=-votes&page=1";
 const topAdventureURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=adventure&page=1";
+  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=comedy&page=1";
 const topActionURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=action&page=1";
+  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=drama&page=1";
 const topRomanceURL =
   "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=romance&page=1";
 
+// submits a fetch request to the API for one page of results
 const fetchData = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
@@ -16,6 +17,8 @@ const fetchData = async (url) => {
   return data;
 };
 
+// submits a fetch request to the API for multiple pages
+// and create the required elements for the categories
 const fetchPages = async (url, cat_name) => {
   let allData = [];
   let data = await fetchData(url);
@@ -31,9 +34,10 @@ const fetchPages = async (url, cat_name) => {
   }
   const sortedList = allData
     .sort((a, z) => parseFloat(z.imdb_score) - parseFloat(a.imdb_score))
-    .slice(0, 7);
+    .slice(0, 7); // sort the results by IMDB score and returns 7 of them;
   const imgURL = sortedList.map((item) => item.image_url);
 
+  // creates the HTMl elements and append them to the body
     function carousel(imgURL, cat_name) {
 
     const carousel_main = document.createElement("div");
@@ -52,7 +56,6 @@ const fetchPages = async (url, cat_name) => {
     prev_btn.classList.add("arrow", "prev");
     prev_btn.setAttribute("id", "previous" + cat_name);
     prev_btn.innerHTML = '&#10094;';
-    // prev_btn.innerHTML='<i class="fa-solid fa-circle-chevron-right"></i>';
 
     next_btn.classList.add("arrow", "next");
     next_btn.setAttribute("id", "next" + cat_name);
@@ -65,21 +68,29 @@ const fetchPages = async (url, cat_name) => {
     carousel_main.appendChild(carousel_container);
 
     document.getElementById("categories").appendChild(carousel_main);
-    // const carousel = document.querySelector("div.carousel");
 
+    // append images url to display images in the carousel
     for (let url of imgURL) {
         const new_img = document.createElement("img");
         new_img.src = url;
+        new_img.classList.add(cat_name);
         new_img.classList.add("img");
+        new_img.style.width = '50%';
+        new_img.style.height = '400px';
+        new_img.style.objectFit = 'cover';
+        new_img.style.margin = '0 13px';
         carousel.appendChild(new_img);
     }
 
     let currentSlide = 0;
-    const slidesToShow = 0.7;
-    const slideWidth = 25;
+    const slidesToShow = 0.5;
+    const slideWidth = 15;
 
+
+    // controls the slideshow
     function showSlide(n) {
-        const slides = document.querySelectorAll(".img");
+
+        const slides = document.getElementsByClassName(cat_name);
         currentSlide = (n + slides.length) % slides.length;
         const offset = -currentSlide * (slidesToShow * slideWidth);
         document.getElementById(
@@ -99,21 +110,19 @@ const fetchPages = async (url, cat_name) => {
 
     // Initial display
     showSlide(currentSlide);
-
 }
 
 carousel(imgURL,cat_name);
 
 };
 
-
-
-
+// submits a fetch request for the top movie and populate the main banner and pop-up window
 const top_movie = fetchData(topMovieURL).then((data) => {
   const top_movie_request = fetchData(data.results[0]["url"]).then(
     (top_movie_data) => {
       const pop_window = document.getElementById("popup-window");
       const details = document.querySelector("ul.top_movie_details");
+      // required details to be displayed in the pop-up window
       const pop_window_details = [
         top_movie_data["title"],
         top_movie_data["genres"],
@@ -128,6 +137,7 @@ const top_movie = fetchData(topMovieURL).then((data) => {
           top_movie_data["worldwide_gross_income"],
         top_movie_data["description"],
       ];
+      // header names following project requirements
       const pop_window_headers = [
         "Title",
         "Full Genre",
@@ -184,7 +194,8 @@ const top_movie = fetchData(topMovieURL).then((data) => {
   );
 });
 
-fetchPages(topSevenURL, "Top Seven Rated Movies");
-fetchPages(topAdventureURL, "Top Seven Adventure Movies");
-fetchPages(topActionURL, "Top Seven Action Movies");
-fetchPages(topRomanceURL, "Top Seven Romance Movies");
+
+fetchPages(topActionURL, "Drama");
+fetchPages(topSevenURL, "Top-Rated");
+fetchPages(topAdventureURL, "Comedy");
+fetchPages(topRomanceURL, "Romance");
