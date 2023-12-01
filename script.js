2af197c1da41af98e0c218af9d53402c5b9a7cbe
GraziMarinoni <grazi.marinoni@gmail.com>
2023-12-01 14:12:28 +0000

    improving JS functions

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const topMovieURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=9.6&sort_by=-votes";
-const topSevenURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=9.3&sort_by=-votes&page=1";
-const topComedyURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=comedy&page=1";
-const topDramaURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=drama&page=1";
-const topRomanceURL =
-  "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=romance&page=1";
+// URLs containing IMDB score and highest number of votes as endpoints for the API request
+const topMovieURL= "http://localhost:8000/api/v1/titles/?imdb_score_min=9.6&sort_by=-votes";
+const topSevenURL= "http://localhost:8000/api/v1/titles/?imdb_score_min=9.3&sort_by=-imdb_score&sort_by=-votes";
 
-// submits a fetch request to the API for one page of results
+// URLs containing IMDB score and movie genre as endpoints for the API request
+const topComedyURL= "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=comedy&sort_by=-imdb_score";
+const topDramaURL= "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=drama&sort_by=-imdb_score";
+const topRomanceURL= "http://localhost:8000/api/v1/titles/?imdb_score_min=8.8&genre=romance&sort_by=-imdb_score";
+
+// header names following project requirements
+const pop_window_headers = [
+    "Title",
+    "Full Genre",
+    "Release date",
+    "MPAA rating",
+    "IMDb score",
+    "Director",
+    "List of actors",
+    "Duration",
+    "Country of origin",
+    "Box Office result",
+    "Movie summary",
+];
+
+// submits a fetch request to the API with promise for one page of results
 async function fetchData(url){
   const response = await fetch(url);
   return await response.json();
@@ -21,30 +34,13 @@ async function fetchPages(url, cat_name, start, end) {
   return new Promise(async (resolve, reject) => {
     try {
       let allData = [];
-      let data = await fetchData(url);
-      allData = allData.concat(data.results);
-
-      while (data.next !== null) {
-        // Create a new URL object by parsing the input URL
-        const nextUrl = new URL(url);
-
-        // Retrieve the current value of the "page" parameter and convert it to an integer
-        let nextPage = parseInt(nextUrl.searchParams.get("page"));
-
-        // Increment the value of nextPage by 1
-        nextPage++;
-
-        // Set the updated nextPage value as the new "page" parameter in the URL
-        nextUrl.searchParams.set("page", nextPage);
-
-        // Convert the updated URL object back to a string and assign it to the variable url
-        url = nextUrl.toString();
-        data = await fetchData(url);
+      // a loop to get the results from page one and two of the API response
+      for (let page = 1; page <= 2; page++) {
+        let data = await fetchData(`${url}&page=${page}`);
         allData = allData.concat(data.results);
       }
 
-      const sortedList = allData
-        .sort((a, z) => parseFloat(z.imdb_score) - parseFloat(a.imdb_score))
+      const sortedList = allData.sort((a, z) => parseFloat(z.imdb_score) - parseFloat(a.imdb_score))
         .slice(start, end);
 
       const imgURL = sortedList.map((item) => item.image_url);
@@ -61,128 +57,80 @@ async function fetchPages(url, cat_name, start, end) {
   });
 }
 
-// Rest of your carousel function remains unchanged
 function carousel(imgURL, cat_name) {
-    const carousel_main = document.createElement("div");
-    const carousel_title = document.createElement("h2");
-    const carousel_container = document.createElement("div");
-    const prev_btn = document.createElement("button");
-    const carousel = document.createElement("div");
-    const next_btn = document.createElement("button");
+  carouselElements (imgURL, cat_name)
+  let currentSlide = 0;
+  const slidesToShow = 0.5;
+  const slideWidth = 36;
 
-    carousel_title.textContent = cat_name;
-    carousel_title.classList.add("category_title");
-    carousel_container.classList.add("container");
-    carousel.classList.add("carousel");
-    carousel.setAttribute("id", cat_name);
-
-    prev_btn.classList.add("arrow", "prev");
-    prev_btn.setAttribute("id", "previous" + cat_name);
-    prev_btn.innerHTML = "&#10094;";
-
-    next_btn.classList.add("arrow", "next");
-    next_btn.setAttribute("id", "next" + cat_name);
-    next_btn.innerHTML = "&#10095;";
-
-    carousel_container.appendChild(prev_btn);
-    carousel_container.appendChild(carousel);
-    carousel_container.appendChild(next_btn);
-    carousel_main.appendChild(carousel_title);
-    carousel_main.appendChild(carousel_container);
-
-    document.getElementById("categories").appendChild(carousel_main);
-
-    // append images url to display images in the carousel
-    for (let url of imgURL) {
-      const new_img = document.createElement("img");
-      new_img.src = url;
-      new_img.classList.add(cat_name);
-      new_img.classList.add("img");
-      carousel.appendChild(new_img);
-    }
-
-    let currentSlide = 0;
-    const slidesToShow = 0.5;
-    const slideWidth = 36;
-
-    // controls the slideshow
-    function showSlide(n) {
-      const slides = document.getElementsByClassName(cat_name);
-      currentSlide = (n + slides.length) % slides.length;
-      const offset = -currentSlide * (slidesToShow * slideWidth);
-      document.getElementById(
-        cat_name
-      ).style.transform = `translateX(${offset}%)`;
-    }
-
-    const previous = document.getElementById("previous" + cat_name);
-    previous.addEventListener("click", function () {
-      showSlide(currentSlide - 1);
-    });
-
-    const next = document.getElementById("next" + cat_name);
-    next.addEventListener("click", function () {
-      showSlide(currentSlide + 1);
-    });
-
-    // Initial display
-    showSlide(currentSlide);
+  // controls the slideshow
+  function showSlide(n) {
+    const slides = document.getElementsByClassName(cat_name);
+    currentSlide = (n + slides.length) % slides.length;
+    const offset = -currentSlide * (slidesToShow * slideWidth);
+    document.getElementById(cat_name).style.transform = `translateX(${offset}%)`;
   }
+
+  const previous = document.getElementById("previous" + cat_name);
+  previous.addEventListener("click", function () {showSlide(currentSlide - 1);
+  });
+
+  const next = document.getElementById("next" + cat_name);
+  next.addEventListener("click", function () {showSlide(currentSlide + 1);
+  });
+
+  // Initial display
+  showSlide(currentSlide);
+}
+
+function carouselElements (imgURL, cat_name){
+  const carousel_main = document.createElement("div");
+  const carousel_title = document.createElement("h2");
+  const carousel_container = document.createElement("div");
+  const prev_btn = document.createElement("button");
+  const carousel = document.createElement("div");
+  const next_btn = document.createElement("button");
+
+  carousel_title.textContent = cat_name;
+  carousel_title.classList.add("category_title");
+  carousel_container.classList.add("container");
+  carousel.classList.add("carousel");
+  carousel.setAttribute("id", cat_name);
+
+  prev_btn.classList.add("arrow", "prev");
+  prev_btn.setAttribute("id", "previous" + cat_name);
+  prev_btn.innerHTML = "&#10094;";
+
+  next_btn.classList.add("arrow", "next");
+  next_btn.setAttribute("id", "next" + cat_name);
+  next_btn.innerHTML = "&#10095;";
+
+  carousel_container.appendChild(prev_btn);
+  carousel_container.appendChild(carousel);
+  carousel_container.appendChild(next_btn);
+  carousel_main.appendChild(carousel_title);
+  carousel_main.appendChild(carousel_container);
+
+  // appending the new elements to the categories section in the HTML
+  document.getElementById("categories").appendChild(carousel_main);
+
+  // appending images url to display images in the carousel
+  for (let url of imgURL) {
+    const new_img = document.createElement("img");
+    new_img.src = url;
+    new_img.classList.add(cat_name);
+    new_img.classList.add("img");
+    carousel.appendChild(new_img);
+  }
+}
 
 // submits a fetch request for the top movie and populate the main banner and pop-up window
 fetchData(topMovieURL).then((data) => {
   fetchData(data.results[0]["url"]).then((top_movie_data) => {
-    const pop_window = document.getElementById("popup-window");
-    const details = document.querySelector("ul.top_movie_details");
-    // required details to be displayed in the pop-up window
-    const pop_window_details = [
-      top_movie_data["title"],
-      top_movie_data["genres"],
-      top_movie_data["date_published"],
-      top_movie_data["rated"],
-      top_movie_data["imdb_score"],
-      top_movie_data["directors"],
-      top_movie_data["actors"],
-      top_movie_data["duration"],
-      top_movie_data["countries"],
-      top_movie_data["usa_gross_income"] +
-        top_movie_data["worldwide_gross_income"],
-      top_movie_data["description"],
-    ];
-    // header names following project requirements
-    const pop_window_headers = [
-      "Title",
-      "Full Genre",
-      "Release date",
-      "MPAA rating",
-      "IMDb score",
-      "Director",
-      "List of actors",
-      "Duration",
-      "Country of origin",
-      "Box Office result",
-      "Movie summary",
-    ];
+    // slicing original image URL to get the full size image
+    let new_url = top_movie_data["image_url"].slice(0, top_movie_data["image_url"].indexOf("@") + 1);
 
-    // adding img element and url to the Top Movie banner
-    const best_movie_img = document.createElement("img");
-    best_movie_img.classList.add("popup_img");
-    let new_url = top_movie_data["image_url"].slice(
-      0,
-      top_movie_data["image_url"].indexOf("@") + 1
-    );
-    best_movie_img.src = new_url;
-    pop_window.appendChild(best_movie_img);
-
-    // adding movie specs to the pop-up window
-    for (let detail in pop_window_details) {
-      const item = document.createElement("li");
-      const text = document.createElement("p");
-      text.textContent =
-        pop_window_headers[detail] + " : " + pop_window_details[detail];
-      item.appendChild(text);
-      details.appendChild(item);
-    }
+    popup(top_movie_data, new_url);
 
     // creating and adding movie details to the banner
     const top_movie_image = document.getElementById("top-movie-img");
@@ -207,6 +155,41 @@ fetchData(topMovieURL).then((data) => {
     });
   });
 });
+
+function popup(data, img_url){
+  const pop_window = document.getElementById("popup-window");
+  const details = document.querySelector("ul.top_movie_details");
+    // required details to be displayed in the pop-up window
+  const pop_window_details = [data["title"],
+      data["genres"],
+      data["date_published"],
+      data["rated"],
+      data["imdb_score"],
+      data["directors"],
+      data["actors"],
+      data["duration"],
+      data["countries"],
+      data["usa_gross_income"] +
+      data["worldwide_gross_income"],
+      data["description"],
+    ];
+
+    // adding img element and url to the Top Movie banner
+    const best_movie_img = document.createElement("img");
+    best_movie_img.classList.add("popup_img");
+
+    best_movie_img.src = img_url;
+    pop_window.appendChild(best_movie_img);
+
+    // adding movie specs to the pop-up window
+    for (let detail in pop_window_details) {
+      const item = document.createElement("li");
+      const text = document.createElement("p");
+      text.textContent = pop_window_headers[detail] + " : " + pop_window_details[detail];
+      item.appendChild(text);
+      details.appendChild(item);
+    }
+}
 
 // Create an array of promises
 const promises = [
